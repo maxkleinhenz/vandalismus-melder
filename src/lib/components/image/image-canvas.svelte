79d23844391f6bebc113d05686363c5eecf256ca
Max Kleinhenz <dev@max-kleinhenz.de>
2025-01-20 22:01:15 +0100

    resize image

--- src/lib/components/image/image-canvas.svelte
+++ src/lib/components/image/image-canvas.svelte
@@ -2,6 +2,10 @@
 	import { type Metadata } from '../helpers/image-metadata';
 	import { onMount } from 'svelte';
 
+	const maxHeight = 1600;
+	const maxWidth = 1200;
+	const defaultFontsize = 48;
+
 	let {
 		image = $bindable(),
 		metadata = $bindable(),
@@ -15,12 +19,40 @@
 	let canvas = $state<HTMLCanvasElement>();
 	let ctx = $state<CanvasRenderingContext2D | null>();
 	let bitmap = $state<ImageBitmap | undefined>();
-	let bitmapWidth = $derived(bitmap?.width ?? 0);
-	let bitmapHeight = $derived(bitmap?.height ?? 0);
+	let bitmapSize = $derived.by(() => {
+		if (!bitmap) return { width: 0, height: 0 };
+
+		var width = bitmap.width;
+		var height = bitmap.height;
+		// Calculate the new dimensions, maintaining the aspect ratio
+		if (width > height) {
+			if (width > maxWidth) {
+				height *= maxWidth / width;
+				width = maxWidth;
+			}
+		} else {
+			if (height > maxHeight) {
+				width *= maxHeight / height;
+				height = maxHeight;
+			}
+		}
+
+		return { width, height };
+
+		// const width = bitmap.width;
+		// const height = bitmap.height;
+
+		// if (width <= maxWidth && height <= maxHeight) {
+		// 	return { width, height };
+		// }
+
+		// const ratio = Math.min(maxWidth / width, maxHeight / height);
+		// return { width: width * ratio, height: height * ratio };
+	});
 
 	onMount(() => {
 		if (!canvas) return;
-		ctx = canvas.getContext('2d');
+		ctx = canvas.getContext('2d', { alpha: false });
 	});
 
 	$effect(() => {
@@ -28,32 +60,30 @@
 
 		createImageBitmap(image).then((b) => {
 			bitmap = b;
-			const width = bitmap?.width;
-			const height = bitmap?.height;
-			ctx!.canvas.width = width;
-			ctx!.canvas.height = height;
 		});
 	});
 
 	$effect(() => {
 		if (!ctx || !bitmap || !metadata) return;
 
-		ctx.clearRect(0, 0, bitmapWidth, bitmapHeight);
+		ctx!.canvas.width = bitmapSize.width;
+		ctx!.canvas.height = bitmapSize.height;
 
-		ctx.drawImage(bitmap, 0, 0, bitmapWidth, bitmapHeight);
+		ctx.clearRect(0, 0, bitmapSize.width, bitmapSize.height);
 
-		const defaultFontsize = 1.5 * (isNaN(metadata.resolution.x) ? 72 : metadata.resolution.x);
-		const fontRatio = defaultFontsize / bitmapWidth;
-		const fontSize = bitmapWidth * fontRatio;
+		ctx.drawImage(bitmap, 0, 0, bitmapSize.width, bitmapSize.height);
+
+		const fontRatio = defaultFontsize / bitmapSize.width;
+		const fontSize = bitmapSize.width * fontRatio;
 		const lineheight = fontSize * 1.5;
 
 		const addressArr = metadata.address ? metadata.address.split('\n') : undefined;
 		const lines = 1 + (addressArr?.length ?? 0);
-		const yStart = bitmapHeight - lines * lineheight;
+		const yStart = bitmapSize.height - lines * lineheight;
 
 		ctx.fillStyle = 'white';
 		ctx.beginPath();
-		ctx.rect(0, yStart, bitmapWidth, bitmapHeight);
+		ctx.rect(0, yStart, bitmapSize.width, bitmapSize.height);
 		ctx.fill();
 
 		const xTextStart = 0.3 * lineheight;
